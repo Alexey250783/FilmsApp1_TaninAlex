6c06d880973906d05ea20423156d31352e4ddc22
AlexeyTanin <tas@konti.su>
2024-12-05 22:18:38 +0300

    created a RecyclerView for the main movie page. I made a blank for the movie details screen based on theCoordinatorLayout, there should be a Collapsing Toolbar and buttons "linked to it" created a screen with movie details

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,7 @@ dependencies {
     implementation(libs.gridlayout.v7)
     implementation(libs.androidx.annotation)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
